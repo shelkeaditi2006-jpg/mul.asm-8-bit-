60df3f4b6beaf22ff503fccc5a1f105569c4f231
shelkeaditi2006-jpg <shelkeaditi2006@gmail.com>
2025-09-20 23:07:17 +0530

    Add files via upload

---
+++ mul.asm
@@ -0,0 +1,18 @@
+global _start
+
+section .data
+    num1 db 0x0A      
+    num2 db 0x05   
+    result dw 0      
+
+section .text
+_start:
+    mov al, [num1]    
+    mov bl, [num2]    
+    mul bl           
+
+    mov [result], ax  
+
+    xor ebx, ebx
+    mov eax, 1
+    int 0x80
